3c0ec9227b0f7b2dd570ea69ce1a28c3897db025
rickujanu76 <rickujanu76@gmail.com>
2024-09-05 16:52:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7241463417:AAErgfWBRJHMo9IBjdWNWv2t0GX_E4jZny0")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("SPIDEY0014")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6392569995")
 
 
 
